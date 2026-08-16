2fdfc239997c2822123c30ccc7be8d11a63a960c
Daniel Quednow <danquednow@gmail.com>
2026-08-16 16:16:49 -0400

    Remove explicit surround import
    I have it already, so I don't need to add it.

--- init.lua
+++ init.lua
@@ -1042,7 +1042,6 @@ require('lazy').setup({
       }
     end,
   },
-  { 'echasnovski/mini.surround', version = false },
 
   -- The following comments only work if you have downloaded the kickstart repo, not just copy pasted the
   -- init.lua. If you want these files, they are in the repository, so you can just download them and
